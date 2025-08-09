08bf6a6fcf018525936ed00f598922e91461c590
Kiran sable <kiransable@outlook.in>
2025-08-09 16:48:42 +0530

    09-08-2025 -3

---
+++ src/test/java/Com/thetestingacdemy/ex_05_TestNgExample/APITesting024_All_Annotations.java
@@ -0,0 +1,20 @@
+package Com.thetestingacdemy.ex_05_TestNgExample;
+
+import org.testng.annotations.*;
+
+public class APITesting024_All_Annotations {
+    @BeforeTest
+    void demo4(){
+        System.out.println("BeforeMethod");
+    }
+
+    @Test
+    void demo5(){
+        System.out.println("Test");
+    }
+
+    @AfterTest
+    void demo6(){
+        System.out.println("AfterMethod");
+    }
+}
